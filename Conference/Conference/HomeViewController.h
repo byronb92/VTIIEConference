fd47ec230a1e14dbda407996df511a1292e40e54
Byron Becker <byronb92@Byrons-MacBook-Pro.local>
2013-10-20 00:20:57 -0400

    Adds homescreen of application with 6 buttons and a timer
    The homescreen of the app consists of buttons labeled About, News, Agenda, Speakers, Contacts, and Sponsors.
    The timer included on the homescreen is a countdown to February 21st, 2014, the day the conference begins.

--- Conference/Conference/HomeViewController.h
+++ Conference/Conference/HomeViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HomeViewController.h
 //  Conference
 //
 //  Created by Byron Becker on 10/19/13.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface HomeViewController : UIViewController
 
 @end
