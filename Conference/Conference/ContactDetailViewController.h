735d369ff4053426257bdce01a147a0d5fc2548b
Byron Becker <byronb92@vt.edu>
2013-12-22 07:13:38 -0500

    Adds Contact Detail View Controller to repository

---
+++ Conference/Conference/ContactDetailViewController.h
@@ -0,0 +1,22 @@
+//
+//  ContactDetailViewController.h
+//  Conference
+//
+//  Created by Byron Becker on 12/21/13.
+//  Copyright (c) 2013 Byron Becker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactDetailViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextView *positionTextView;
+@property (strong, nonatomic) IBOutlet UITextView *emailTextView;
+@property (strong, nonatomic) IBOutlet UITextView *phoneTextView;
+@property (strong, nonatomic) IBOutlet UIImageView *photoImageView;
+
+// Once a contact has been clicked on from the table view, their information is
+// passed to the ContactDetailViewController.
+@property (strong, nonatomic) NSMutableDictionary *contactData;;
+
+@end
